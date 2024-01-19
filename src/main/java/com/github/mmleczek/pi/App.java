a36a107bff22515e7a0d0953fbb9252ec08ceba5
mmleczek <37123369+mmleczek@users.noreply.github.com>
2024-01-19 17:05:54 +0100

    Add initial code for the homework
    The next task is to make it multithreaded in Java.

--- src/main/java/com/github/mmleczek/pi/App.java
+++ src/main/java/com/github/mmleczek/pi/App.java
@@ -1,7 +1,20 @@
 package com.github.mmleczek.pi;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
-
+        int attPoints = 2000000000;
+        int pointsInCircle = 0;
+        Random random = new Random();
+        for(int i = 0; i < attPoints; i++) {
+            double x = random.nextDouble();
+            double y = random.nextDouble();
+            double distanceFromCenter = Math.sqrt(x * x + y * y);
+            if (distanceFromCenter < 1) {
+                pointsInCircle++;
+            }
+        }
+        double pi = ((double)pointsInCircle * 4) / ((double)attPoints);
+        System.out.println(pi);
     }
 }
